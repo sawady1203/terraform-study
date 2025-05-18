7f32843e7820c67ec71e39d95f86638a283b412c
sawady1203 <sawady.dev@gmail.com>
2025-05-18 18:01:28 +0900

    variable compute_instance_zone

--- basic/main.tf
+++ basic/main.tf
@@ -3,10 +3,22 @@ provider "google" {
   region  = "asia-northeast1"
 }
 
+variable "compute_instance_zone" {
+  description = "The zone to deploy the compute instance"
+  type        = string
+  default     = "asia-northeast1-a"
+
+  validation {
+    condition     = contains(["asia-northeast1-a", "asia-northeast1-b", "asia-northeast1-c"], var.conpute_instance_zone)
+    error_message = "The zone must be one of the following: asia-northeast1-a, asia-northeast1-b, asia-northeast1-c"
+  }
+  
+}
+
 resource "google_compute_instance" "default" {
   name         = "test"
   machine_type = "e2-micro"
-  zone         = "asia-northeast1-a"
+  zone         = var.compute_instance_zone
 
   boot_disk {
     initialize_params {
