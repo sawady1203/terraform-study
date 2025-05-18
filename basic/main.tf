5d291021281fd10f80780d242147fafa0ccbd155
sawady1203 <sawady.dev@gmail.com>
2025-05-18 16:59:37 +0900

    compute engine をつくってみる

---
+++ basic/main.tf
@@ -0,0 +1,20 @@
+provider "google" {
+  project = "terraform-practice-book"
+  region  = "asia-northeast1"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "f1-micro"
+  zone         = "asia-northeast1-c"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+  } 
+}
